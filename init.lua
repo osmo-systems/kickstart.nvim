9e1ad10e63f6d883afbd93bd63e34295b12cada6
Mathieu Antoine <mathieuantoine4@gmail.com>
2025-08-14 03:58:31 +0200

    initial

--- init.lua
+++ init.lua
@@ -90,7 +90,6 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
 -- [[ Setting options ]]
@@ -736,6 +735,26 @@ require('lazy').setup({
     end,
   },
 
+  -- Themes
+  { 'rebelot/kanagawa.nvim', priority = 1000 },
+  { 'rose-pine/neovim', priority = 1000 },
+  { -- Switch dark/light theme based on system setting.
+    'f-person/auto-dark-mode.nvim',
+    config = function()
+      require('auto-dark-mode').setup {
+        update_interval = 5000,
+        set_dark_mode = function()
+          vim.opt.background = 'dark'
+          vim.cmd 'colorscheme kanagawa'
+        end,
+        set_light_mode = function()
+          vim.opt.background = 'light'
+          vim.cmd 'colorscheme rose-pine-dawn'
+        end,
+      }
+    end,
+  },
+
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
@@ -874,28 +893,6 @@ require('lazy').setup({
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
     },
-  },
-
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
   },
 
   -- Highlight todo, notes, etc in comments
