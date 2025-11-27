173f204a7b5dd2d134f3285048c819f3b1efd45f
Mathieu Antoine <mathieuantoine4@gmail.com>
2025-11-27 20:40:50 +0100

    added gh plugin

---
+++ lua/custom/plugins/github.lua
@@ -0,0 +1,38 @@
+return {
+  'folke/snacks.nvim',
+  opts = {
+    gh = {
+      -- your gh configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    picker = {
+      sources = {
+        gh_issue = {
+          -- your gh_issue picker configuration comes here
+          -- or leave it empty to use the default settings
+        },
+        gh_pr = {
+          -- your gh_pr picker configuration comes here
+          -- or leave it empty to use the default settings
+        },
+      },
+    },
+  },
+  keys = {
+    {
+      '<leader>gp',
+      function()
+        Snacks.picker.gh_pr()
+      end,
+      desc = 'GitHub Pull Requests (open)',
+    },
+    {
+      '<leader>gP',
+      function()
+        Snacks.picker.gh_pr { state = 'all' }
+      end,
+      desc = 'GitHub Pull Requests (all)',
+    },
+  },
+}
