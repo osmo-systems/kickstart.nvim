703dd57d6e930213fe58e7d57ad6b428905e09a6
Mathieu Antoine <mathieuantoine4@gmail.com>
2025-11-29 20:05:06 +0100

    updated git.lua custom plugin

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,8 +1,60 @@
 return {
-  'tpope/vim-fugitive',
-  config = function()
-    -- Optional keymaps
-    vim.keymap.set('n', '<leader>gs', ':Git<CR>', { desc = 'Git status' })
-    vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { desc = 'Git blame' })
-  end,
+  {
+    'tpope/vim-fugitive',
+    config = function()
+      -- Optional keymaps
+      vim.keymap.set('n', '<leader>gs', ':Git<CR>', { desc = 'Git status' })
+      vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { desc = 'Git blame' })
+    end,
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    lazy = false,
+    keys = {
+      {
+        '<leader>gsd',
+        '<cmd>Gitsigns diffthis main<cr>',
+      },
+      opts = {
+        current_line_blame = true,
+      },
+    },
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol',
+      },
+    },
+  },
+  {
+    'sindrets/diffview.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    cmd = {
+      'DiffviewOpen',
+      'DiffviewClose',
+      'DiffviewToggleFiles',
+      'DiffviewFocusFiles',
+      'DiffviewFileHistory',
+    },
+    opts = {
+      view = {
+        merge_tool = {
+          layout = 'diff3_mixed',
+        },
+      },
+      file_panel = {
+        listing_style = 'tree',
+      },
+      default_args = {
+        DiffviewOpen = { '--imply-local' },
+      },
+      hooks = {
+        diff_buf_read = function(bufnr)
+          vim.opt_local.wrap = true
+          vim.opt_local.linebreak = true
+        end,
+      },
+    },
+  },
 }
